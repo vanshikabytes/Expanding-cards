8f184be56bee2d051fb7585fb98ee5f86ea13426
Vanshika Vijay <161073639+vanshikabytes@users.noreply.github.com>
2024-03-23 23:48:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels=document.querySelectorAll('.pannel')
+panels.forEach(pannel=> {
+    pannel.addEventListener('click',() => {
+        removeActiveClasses()
+        pannel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(pannel=> {
+    pannel.classList.remove('active')
+    })
+}
